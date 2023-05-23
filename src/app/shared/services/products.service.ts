c0eb4ba651aff9c90c0259a1856e786ddb0426b4
Lur81 <l.cano.soler@gmail.com>
2023-05-23 15:44:43 +0200

    environments

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,6 +1,7 @@
 import { IProduct } from './../../models/product.interface';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,23 +11,23 @@ export class ProductsService {
   constructor(private http: HttpClient) { }
 
   getProducts() {
-    return this.http.get("http://localhost:3000/products")
+    return this.http.get(environment.url + 'products')
   }
 
   postProducts(newProduct: IProduct) {
-    this.http.post("http://localhost:3000/products", newProduct).subscribe()
+    this.http.post(environment.url + 'products', newProduct).subscribe()
   }
 
   getProductsById(id: number) {
-    return this.http.get(`http://localhost:3000/products/${id}`)
+    return this.http.get(environment.url + 'products/' + id)
   }
 
   putProducts(newProduct: IProduct) {
-    this.http.put(`http://localhost:3000/products/${newProduct.id}`, newProduct).subscribe()
+    this.http.put(environment.url + 'products/' + newProduct.id, newProduct).subscribe()
   }
 
   deleteProducts(id: number) {
-    this.http.delete(`http://localhost:3000/products/${id}`).subscribe()
+    this.http.delete(environment.url + 'products/' + id).subscribe()
   }
 
 }
---
+++ src/environments/environments.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    production: true,
+    url: 'https://project-angular-backend-pi.vercel.app/'
+  };
